d60549cacdbc74ff90cbe944ce45f412b9cbf787
ainamoncho <72893069+ainamoncho@users.noreply.github.com>
2022-12-03 22:45:12 +0100

    ad

---
+++ webapp.py
@@ -0,0 +1,45 @@
+import streamlit as st
+import pandas as pd
+from streamlit_option_menu import option_menu
+from add_delivery import add_new_delivery
+#from ML_prediction import ML_prediction
+#from global_interpretation import global_interpretation
+
+st.set_page_config(
+    page_title='DELIVERIES',
+    page_icon='🏥',
+    layout='wide',
+    initial_sidebar_state='expanded',
+)
+
+st.title('🏥 DELIVERIES')
+
+with st.sidebar:
+    page = option_menu('Main Menu', ['Home', 'Add New Delivery', 'Machine Learning Prediction', 'Global Interpretation'], 
+        icons=['house', 'bag-plus', 'arrow-bar-up', 'bar-chart-line'], menu_icon="cast", default_index=1,
+        styles={
+        "container": {"padding": "0!important", "background-color": "#fafafa"},
+        "icon": {"color": "red", "font-size": "20px"}, 
+        "nav-link": {"font-size": "15px", "text-align": "left", "margin":"0px", "--hover-color": "#eee"},
+        "nav-link-selected": {"background-color": "grey"},
+    })
+
+data = pd.read_csv('stage_1_balanced.csv', sep=';')
+
+if 'data' not in st.session_state:
+    st.session_state.data=data
+
+
+if page == 'Home':
+    if st.checkbox('Display data'):
+        st.write(st.session_state.data)
+
+if page == 'Add New Delivery':
+    add_new_delivery()
+
+if page == 'Machine Learning Prediction':
+    #ML_prediction()
+    st.write('ML')
+
+if page == 'Global Interpretation':
+    st.write('Global Interpretation')
